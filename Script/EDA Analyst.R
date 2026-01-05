b196607e795f9609621bf92e0442be0d98d1ecae
vidhyapar <160858260+vidhyapar@users.noreply.github.com>
2026-01-05 18:59:52 +0530

    Add files via upload

---
+++ Script/EDA Analyst.R
@@ -0,0 +1,14 @@
+data <- read_excel("streaming_cleaned.xlsx")
+
+data$skip_30s <- factor(data$skip_30s, levels = c(0, 1),
+                        labels = c("Not Skipped", "Skipped"))
+
+# Distribution analysis
+ggplot(data, aes(energy, fill = skip_30s)) +
+  geom_histogram(alpha = 0.6, bins = 30) +
+  theme_minimal()
+
+# Outlier & spread analysis
+ggplot(data, aes(skip_30s, loudness)) +
+  geom_boxplot() +
+  theme_minimal()
